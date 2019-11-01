8f2214016c7a9284bde17578e136799c4c614d4c
unknown <ptjh0309@hanmail.net>
2019-11-01 21:34:32 +0900

    Make TicketingView

---
+++ src/views/TicketingView1.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width : 80%;
+    margin : 0 auto;
+`;
+
+const TicketingView1 = () => (
+    <Container>
+        예매 창 1
+    </Container>
+);
+
+export default TicketingView1;
---
+++ src/views/TicketingView3.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width : 80%;
+    margin : 0 auto;
+`;
+
+const TicketingView3 = () => (
+    <Container>
+        예매 창 3
+    </Container>
+);
+
+export default TicketingView3;
