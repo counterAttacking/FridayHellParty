85be99c7e37e4fbac30449c0c25710ae42b122eb
jihunsg <rlawlgns51796@gmail.com>
2019-11-08 01:30:18 +0900

    TicketingView1

--- src/views/TicketingView1.js
+++ src/views/TicketingView1.js
@@ -4,11 +4,24 @@ import styled from 'styled-components';
 const Container = styled.div`
     width : 80%;
     margin : 0 auto;
+    
 `;
+
 
 const TicketingView1 = () => (
     <Container>
-        예매 창 1
+        <form>
+
+        <fieldset>
+            공연날짜 <input type="none" value ="2019-10-31-19:00" readOnly/>
+            가격 <input type="none" value ="15000" readOnly/>원
+            <p align="center">공연정보</p>
+            <div>
+                <img src={"http://ticketimage.interpark.com/Play/image/large/19/19016399_p.gif"} align="center" />
+            </div>
+            <button formAction="/TicKetingView2" align="center" >예매하기</button>
+        </fieldset>
+        </form>
     </Container>
 );
 
