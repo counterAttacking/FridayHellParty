436629ed014543ac35836c8a6eebf70fd6a4841b
jihunsg <rlawlgns51796@gmail.com>
2019-11-13 20:35:03 +0900

    아이디찾기 -> 비밀번호 찾기

--- src/views/MemberRegisterView.js
+++ src/views/MemberRegisterView.js
@@ -5,15 +5,7 @@ const Container = styled.div`
     width : 80%;
     margin : 0 auto;
 `;
-const Input1 =styled.input`
-    dispaly:inline;
-`;
-const Mli = styled.li`
-    display:inline;
-`;
-const MSPan=styled.span`
-    float:left;
-`;
+
 const MemberRegisterView = () => (
     <Container>
         <span>
@@ -25,9 +17,9 @@ const MemberRegisterView = () => (
             <a href="/FindMyIdView">아이디 찾기 </a><a href="/FindMyPasswdView"> 비밀번호 찾기</a>
         </div>
         <fieldset>
-            <ul><Mli>ID</Mli><br/><Mli>password</Mli></ul><span><Input1 id="ID" type="text" placeholder="아이디 입력"/></span>
+            <span>ID</span><input id="ID" type="text" placeholder="아이디 입력"/>
             <button>중복확인</button><br/>
-            <input id="password" type="password"  placeholder="비밀번호 입력"/><br/>
+            <span>password</span><input id="password" type="password"  placeholder="비밀번호 입력"/><br/>
             <span>이름 </span><input id="name" type="text"  placeholder="이름 입력"/><br/>
             <span>생년월일 </span><input id="birth" type="text"  placeholder="생년월일"/><br/>
             <span>휴대전화번호 </span><input id="phoneNum" type="text"  placeholder="휴대폰 번호"/><br/>
