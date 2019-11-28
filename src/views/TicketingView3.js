0ee913d932d689bdf89ce0b0bf97c7a5f05540a8
unknown <ptjh0309@hanmail.net>
2019-11-28 17:46:28 +0900

    Apply CSS.

--- src/views/TicketingView3.js
+++ src/views/TicketingView3.js
@@ -1,33 +1,81 @@
-﻿import React from 'react';
-import styled from 'styled-components';
+﻿import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
 
 const Container = styled.div`
     width : 80%;
     margin : 0 auto;
+    text-align : center;
 `;
 
-const TicketingView3 = () => (
-    <Container>
-        <form>
-            <fieldset>
-                <legend>예약자 확인</legend>
-                이름 <input type="text" placeholder="김철수" readOnly /><br />
-                생년월일 <input type="text" placeholder="91.01.01" readOnly /><br />
-                휴대전화번호 <input type="tel" placeholder="010-1577-1577" readOnly /><br />
-                이메일 <input type="email" placeholder="sample@sample.net" readOnly /><br />
-                결제 금액 <input type="text" placeholder="15,000원" readOnly /><br />
-                예약 정보 <input type="text" placeholder="2019년 10월 31일 19시 1열 10번" readOnly /><br />
-                결제 금액
-                <select name="pay">
-                    <option value="creditCart">신용카드</option>
-                    <option value="accountTransfer">계좌이체</option>
-                    <option value="banklessDeposit">무통장 입금</option>
-                    <option value="kakaoPay">카카오 페이</option>
-                </select><br />
-                <input type="button" value="결제하기" /> <input type="button" value="결제 취소" />
-            </fieldset>
-        </form>
-    </Container>
-);
+const ReservedSection = styled.section`
+    & section {
+        margin : 20px 0;
+    }
+
+    & label {
+        width : 200px;
+        display : inline-block;
+        text-align : left;
+        font-size : 120%;
+    }
+
+    & input {
+        height : 45px;
+        font-size : 120%;
+        font-weight : bold;
+    }
+
+    & button {
+        width : 200px; height : 55px;
+        font-size : 200%;
+        background : #22b8cf;
+        border : 0;
+        border-radius : 5px;
+        color : #ffffff;
+        margin : 20px 50px;
+    }
+`;
+
+class TicketingView3 extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Container>
+                <section>
+                    <ReservedSection>
+                        <section>
+                            <label>이름</label><input type="text" placeholder="김철수" readOnly />
+                        </section>
+                        <section>
+                            <label>생년월일</label><input type="text" placeholder="91.01.01" readOnly />
+                        </section>
+                        <section>
+                            <label>휴대전화번호</label><input type="tel" placeholder="010-1577-1577" readOnly />
+                        </section>
+                        <section>
+                            <label>이메일</label><input type="email" placeholder="sample@sample.net" readOnly />
+                        </section>
+                        <section>
+                            <label>결제 금액</label><input type="text" placeholder="15,000원" readOnly />
+                        </section>
+                        <section>
+                            <label>예약 정보</label><input type="text" placeholder="2019년 10월 31일 19시 1열 10번" readOnly />
+                        </section>
+                        <section>
+                            <label>결제 금액</label><input type="text" placeholder="결제 방법을 선택하세요." />
+                        </section>
+                        <section>
+                            <button>확인</button>
+                            <button>결제 취소</button>
+                        </section>
+                    </ReservedSection>
+                </section>
+            </Container>
+        )
+    }
+}
 
 export default TicketingView3;
