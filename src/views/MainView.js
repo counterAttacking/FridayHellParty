6fe072b827755306687f5559eb53625302684ddf
counterAttacking <45085586+counterAttacking@users.noreply.github.com>
2020-03-06 19:22:56 +0900

    Update MainView.js

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -232,62 +232,58 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
   //   return data;
   // }
  
-export default function AuthExample () {  
-  // var CC2 = [];
-  // CC2 = getconcert();
-  // console.log(CC2);
+export default function AuthExample () {
     return (
-      <div>
-  <Container>
-                <header>
-                 <Logo>
-                       <Link1 href="/">로고</Link1>
-                       
-                    </Logo>
-                    <Box>
-                        <Lgnb>
+        <Container>
+            <header>
+                <Logo>
+                    <Link1 href="/">로고</Link1>
+                </Logo>
+                <Box>
+                    <Lgnb>
                         <Router>
                             <Logindiv>
-                                <AuthButton/>
+                                <AuthButton />
                             </Logindiv>
                         </Router>
-                        </Lgnb>
-                     
-                    </Box>
-                </header>
+                    </Lgnb>
+                </Box>
+            </header>
+            <section>
                 <section>
-                    <div>
-                        <img src={"http://ticketimage.interpark.com/TCMS4/Main/201912/MainVisual_248b4c8d-44b6-4617-860b-64f2870e6b4f.png"} alt={"메인 배너"} width="100%" />  {/* 이미지가 실시간으로 변경 */}
-                        
-                 </div>
-                    <div>
-                      <br />
-                     <a href="/TicketingView1?concertname=2020 THE 신승훈 SHOW">
-                            <img src={"http://ticketimage.interpark.com/TCMS4/Main/202001/MainVisual_6e60f36a-b989-47f6-8cc5-1b885c699874.png"} alt={"이미지가 없어요"} />
-                     </a>
-                        <a href="/TicketingView1?concertname=2020 Theatre 이문세">
-                         <img src={"http://ticketimage.interpark.com/Play/image/large/19/19011808_p.gif"} alt={"이미지가 없어요"} />
-                     </a>
-                     <a href="/TicketingView1?concertname=2020 THE 신승훈 SHOW">
-                            <img src={"http://ticketimage.interpark.com/Play/image/large/19/19011716_p.gif"} alt={"이미지가 없어요"} />
-                     </a>
-                        <br />
-                        <a href="/TicketingView1?concertname=2020 Theatre 이문세">
-                            <img src={"http://ticketimage.interpark.com/TCMS3.0/CO/HOT/1910/191030034415_19016188.gif"} alt={"이미지가 없어요"} />
-                     </a>
-                     <a href="/TicketingView1?concertname=2020 THE 신승훈 SHOW">
-                           <img src={"http://ticketimage.interpark.com/TCMS3.0/CO/HOT/1910/191014115354_19014994.gif"} alt={"이미지가 없어요"} />
-                        </a>
-                        <a href="/TicketingView1?concertname=2020 Theatre 이문세">
-                            <img src={"http://ticketimage.interpark.com/TCMS3.0/CO/HOT/1910/191031112636_19016101.gif"} alt={"이미지가 없어요"} />
-                     </a>
-                    </div>
+                    <a href="/TicketingView1/1">
+                        <img src={"http://ticketimage.interpark.com/TCMS4/Main/201912/MainVisual_578b59f7-27db-4459-84a7-c6548d557581.jpg"} width="100%" />
+                    </a>
                 </section>
-                <footer align="center">
-                    <h3>Copyright FridayHellParty. All rights reserved.</h3>
-                </footer>
-            </Container>
-      
-      </div>
+                <section>
+                    <a href="/TicketingView1/2">
+                        <img src={"http://ticketimage.interpark.com/TCMS4/Main/202001/MainVisual_a302e2f2-0551-4bd8-be19-a393bafbc5ef.jpg"} width="100%" />
+                    </a>
+                </section>
+                <section>
+                    <a href="/TicketingView1/3">
+                        <img src={"http://ticketimage.interpark.com/TCMS4/Main/202002/MainVisual_8a0e0652-f863-4b83-8ea0-774bad09882a.jpg"} width="100%" />
+                        </a>
+                </section>
+                <section>
+                    <a href="/TicketingView1/4">
+                        <img src={"http://ticketimage.interpark.com/TCMS4/Main/202002/MainVisual_1df1da58-70b9-45e4-8754-8c69a5420062.jpg"} width="100%" />
+                    </a>
+                </section>
+                <section>
+                    <a href="/TicketingView1/5">
+                        <img src={"http://ticketimage.interpark.com/TCMS4/Main/202002/MainVisual_61d4e856-8af1-48f2-a04a-f43bcfd412e2.jpg"} width="100%" />
+                    </a>
+                </section>
+                <section>
+                    <a href="/TicketingView1/6">
+                        <img src={"http://ticketimage.interpark.com/TCMS4/Main/201909/MainVisual_a8d08402-654d-4ede-b637-c597d1512dc5.jpg"} width="100%" />
+                    </a>
+                </section>
+            </section>
+            <footer align="center">
+                <h3>Copyright FridayHellParty. All rights reserved.</h3>
+            </footer>
+        </Container>
     )
-  }
+}
