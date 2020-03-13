43046efb69f927c1cbd3aee8e17d53122ad272c3
KimMyoungSeop <49678602+KimMyoungSeop@users.noreply.github.com>
2020-03-13 22:01:10 +0900

    table 정리, map

--- src/views/MyReserveView.js
+++ src/views/MyReserveView.js
@@ -8,12 +8,46 @@ const Container = styled.div`
     text-align : center;
 `;
 
+const Table = styled.table`
+border-collapse: collapse;
+    text-align: left;
+    line-height: 1.5;
+    border: 1px solid #ccc;
+    margin: 20px 10px;
+
+`;
+
+const Thead = styled.thead`
+border-right: 1px solid #ccc;
+border-left: 1px solid #ccc;
+background: #e7708d;
+
+`;
+const TheadTh = styled.th`
+width: 150px;
+padding: 10px;
+font-weight: bold;
+vertical-align: top;
+color: #fff;
+background: #e7708d;
+margin: 20px 10px;
+
+`;
+const Td = styled.td`
+width: 350px;
+padding: 10px;
+vertical-align: top;
+border-bottom: 1px solid #ccc;
+
+`;
+
+
+
 class MyReserveView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userId: JSON.parse(sessionStorage.getItem('plainUserId')),
-            reserveData: [],
+           
         }
     }
 
@@ -36,38 +70,20 @@ class MyReserveView extends React.Component {
         const { reserveData } = this.state;
         return (
             <Container>
-                <section>
-                    <label>예약 번호</label>
-                    <label></label>
-                </section>
-                <section>
-                    <label>공연명</label>
-                    <label></label>
-                </section>
-                <section>
-                    <label>공연 날짜</label>
-                    <label></label>
-                </section>
-                <section>
-                    <label>예매자</label>
-                    <label></label>
-                </section>
-                <section>
-                    <label>예약 시간</label>
-                    <label></label>
-                </section>
-                <section>
-                    <label>선택 좌석</label>
-                    <label></label>
-                </section>
-                <section>
-                    <label>가격</label>
-                    <label></label>
-                </section>
-                <section>
-                    <label>결제 방법</label>
-                    <label></label>
-                </section>
+               
+                <div align='center'>
+                    <Table>
+                        <Thead>    
+                            <TheadTh>예약번호</TheadTh>
+                            <TheadTh>공연명</TheadTh>
+                            <TheadTh>공연날짜</TheadTh>
+                            <TheadTh>예매수</TheadTh>
+                        </Thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </Table>
+                </div>
             </Container>
         );
     }
